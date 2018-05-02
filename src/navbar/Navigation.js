1e560e538a0c8cc9ba11f751a2a3f0b6c7e8738a
wurikiji <wurikiji@gmail.com>
2018-05-03 06:52:02 +0900

    Add Bootstrap nav
    navigation에 Bootstrap 적용

--- src/navbar/Navigation.js
+++ src/navbar/Navigation.js
@@ -1,40 +1,71 @@
 import React, {Component} from 'react';
 import './navigation.css';
+import logo from '../logo.svg';
 
 const $ = window.$;
 const navItem = [
     {
         name: '키워드',
+        label: 'nav-keywords-tab',
         page: './index.html',
+        control: 'nav-keywords-doc'
     },
     {
         name: '선물 추천',
+        label: 'nav-gifts-tab',
         page: './index.html',
+        control: 'nav-gifts-doc'
     },
     {
         name: '친구들',
+        label: 'nav-friends-tab',
         page: './index.html',
+        control: 'nav-friends-doc'
     },
     {
         name: '계좌번호',
+        label: 'nav-account-tab',
         page: './index.html',
+        control: 'nav-account-doc'
     }
 ];
 
 class NavItem  extends Component {
     render() {
-        let className="flex-sm-fill text-sm-center nav-link ";
-        return this.props.menu.map((val ,idx, arr) => (<a className={idx==0?className + " active": className} href="#" key={val.name}>{val.name}</a>));
+        let className="nav-link nav-item";
+        return this.props.menu.map((val ,idx, arr) => (
+                        <a id={val.label} 
+                            role="tab" data-toggle="tab" 
+                            className={idx===0?className + " active": className} 
+                            href={"#" + val.control} key={val.name} 
+                            aria-controls={val.control}>
+                            {val.name}
+                        </a>
+                        )
+                    );
     }
 }
 
 class Navigation extends Component {
     render() {
         return (
-            <nav className="mainNav navbar nav-tabs flex-column flex-sm-row">
-                <a href="#" className="navbar-brand">TiFy</a>
-                {<NavItem menu={navItem}></NavItem>}
-            </nav>
+            <React.Fragment>
+                <nav id="nav-tab" className="mainNav">
+                    <div className="nav nav-tabs nav-fill" role="tablist">
+                        <a href="#" className="navbar-brand">
+                            <img src={logo} alt="tify logo" className="d-inline-block" width="64" height="64"/>
+                            tify
+                        </a>
+                        {<NavItem menu={navItem}></NavItem>}
+                    </div>
+                </nav>
+                <div className="tab-content">
+                    <div id='nav-keywords-doc' className="tab-pane fade show active" role="tabpanel" aria-labelledby='nav-keywords-tab'> keywords tab</div>
+                    <div id='nav-gifts-doc' className="tab-pane fade" role="tabpanel" aria-labelledby='nav-gifts-tab'> gifts tab</div>
+                    <div id='nav-friends-doc' className="tab-pane fade" role="tabpanel" aria-labelledby='nav-friends-tab'> friends tab</div>
+                    <div id='nav-account-doc' className="tab-pane fade" role="tabpanel" aria-labelledby='nav-account-tab'> account tab</div>
+                </div>
+            </React.Fragment>
         )
     }
 }
